51cabb2a31729dfdefd66c1b22df2c16e9babe38
lovenesh <lovenesh73@gmail.com>
2025-05-27 22:43:46 -0700

    commit

--- queue.py
+++ queue.py
@@ -9,3 +9,15 @@ class queue:
         front=self.values[0]
         self.values=self.values[1:]
         return front
+    
+q=queue()
+q.enqueue(10)
+print(q.values)
+q.enqueue(20)
+print(q.values)
+q.enqueue(30)
+print(q.values)
+q.enqueue(40)
+print(q.values)
+q.enqueue(50)
+print(q.values)
