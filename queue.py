0216295cb82a5c1b18fceb70e15a7ffac4586160
lovenesh <lovenesh73@gmail.com>
2025-05-27 22:41:12 -0700

    queue

--- queue.py
+++ queue.py
@@ -1,6 +1,11 @@
 class queue:
     def __init__(self):
         self.values = []
-        
+
     def enqueue(self,x):
         self.values.append(x)
+
+    def dequeue(self):
+        front=self.values[0]
+        self.values=self.values[1:]
+        return front
