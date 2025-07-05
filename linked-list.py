26f3eb3166d4a005de91e1979fbda94b54b4aa50
lovenesh <lovenesh73@gmail.com>
2025-07-05 17:37:43 +0530

    commit

--- linked-list.py
+++ linked-list.py
@@ -17,6 +17,29 @@ class linkedlist:
         while tmp.next != None:
             tmp=tmp.next
         tmp.next=new_node
+
+    def insert_at_k(self,data,k):
+        if self.head == None:
+            if k == 1:
+                new_node=Node(data)
+                return new_node
+            else:
+                return None
+        if k == 1:
+            new_node=Node(data)
+            new_node.next=self.head
+            self.head=new_node
+        if k > 1:
+            count=1
+            tmp=self.head
+            while tmp != None:
+                if count == k-1:
+                    new_node=Node(data)
+                    new_node.next=tmp.next
+                    tmp.next=new_node
+                    break
+                tmp=tmp.next
+                count+=1
     
     def Print(self):
         tmp =self.head
@@ -30,4 +53,8 @@ ll.insert_at_head(30)
 ll.insert_at_head(20)
 ll.insert_at_head(10)
 ll.insert_at_last(40)
+
+
+
+ll.insert_at_k(25,3)
 ll.Print()
