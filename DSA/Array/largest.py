e1ae00445878c9908336951fccda9b047216a79c
lovenesh <lovenesh73@gmail.com>
2025-08-27 17:04:12 +0530

    commit

--- DSA/Array/largest.py
+++ DSA/Array/largest.py
@@ -4,8 +4,11 @@ class solution:
         for i in range(0,len(num)):
             if num[i] >= greatest:
                 greatest=num[i]
-        return greatest
-    
+        slargest=0
+        for i in range(0,len(num)):
+            if num[i]>=slargest and num[i] != greatest:
+                slargest=num[i]
+        return slargest    
 s=solution()
 num=[1,4,5,3,4,9]
 print(s.largest(num))
